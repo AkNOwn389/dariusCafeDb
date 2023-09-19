553fbeaaded2d2c2ff6aeedb5f92d4adc1ea051a
black <63425602+AkNOwn389@users.noreply.github.com>
2023-09-20 03:01:28 +0800

    Update urls.py

--- core/urls.py
+++ core/urls.py
@@ -24,7 +24,7 @@ urlpatterns = [
     path('', admin.site.urls),
     path('api/feedback', feedback_api),
     path('api/get-feedback', get_user_feedback),
-]
+]+ static(settings.MEDIA_URL, document_root = settings.MEDIA_ROOT) + static(settings.STATIC_URL, document_root = settings.STATIC_ROOT)
 
 if settings.DEBUG:
     urlpatterns += static(settings.MEDIA_URL, document_root = settings.MEDIA_ROOT)
