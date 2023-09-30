deaa4b55de0d92dbf51cc655d460cefaa1636e7d
black <63425602+AkNOwn389@users.noreply.github.com>
2023-09-30 15:42:45 +0800

    Update urls.py

--- core/urls.py
+++ core/urls.py
@@ -5,15 +5,16 @@ from django.conf.urls.static import static
 from django.conf import settings
 
 urlpatterns = [
-    
+
     path('feedback/', feedback_api),
     path('get-feedback/', get_user_feedback),
     path('admin', admin.site.urls),
-    
-    
+
+
 ]
 
 if settings.DEBUG:
-    urlpatterns += static(settings.MEDIA_URL, document_root = settings.MEDIA_ROOT)
-    urlpatterns += static(settings.STATIC_URL, document_root = settings.STATIC_ROOT)
-
+    urlpatterns += static(settings.MEDIA_URL,
+                          document_root=settings.MEDIA_ROOT)
+    urlpatterns += static(settings.STATIC_URL,
+                          document_root=settings.STATIC_ROOT)
